48530307487f570a8b98c2a2da3d8d16571544e0
OscarJ <lijantropique@protonmail.com>
2018-06-18 21:22:50 -0400

    Add testing file Ch2

---
+++ T02StartingOut.hs
@@ -0,0 +1,13 @@
+import M02StartingOut
+
+main = print (penultimate [0,1] == 0)
+-- findK 2 [0,0,1,0,0,0] == 1
+-- isPalindrome "allo"
+-- isPalindrome "abba"
+-- duplicate [1,2,3]== [1,1,2,2,3,3]
+-- ziplike [1,2,3] ['a','b','c', 'd'] == [(1,'a'), (2, 'b'), (3, 'c')]
+-- splitAtIndex 3 [1,1,1,2,2,2] == ([1,1,1],[2,2,2])
+-- dropK 3 [0,0,0,1,0,0,0] == [0,0,0,0,0,0]
+-- slice 3 6 [0,0,0,1,2,3,0,0,0] == [1,2,3]
+-- insertElem 2 5 [0,0,0,0,0,0] == [0,0,0,0,0,2,0]
+-- rotate 2 [1,2,3,4,5] == [3,4,5,1,2]
